df2f9e5fc188e9c6c43a071dcdafaf84ae79e4f0
mina bessada <48674093+mina-adibe@users.noreply.github.com>
2021-01-02 17:30:19 +0200

    configurestore - connect - create store

--- src/components/redux/configureStore.js
+++ src/components/redux/configureStore.js
@@ -1,1 +1,19 @@
-import { createStore } from "redux";
+//this seems sucks - good news u will do this once
+//create store
+
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducers from "./reducers";
+//force u to not mutate the state
+import reduxImmutableStaleInvariant from "redux-immutable-state-invariant";
+
+//redux middleware is a way to enhance redux behavior with extra functionality
+export default function congigureStore(initialState) {
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+  return createStore(
+    rootReducers,
+    initialState,
+    composeEnhancers(applyMiddleware(reduxImmutableStaleInvariant()))
+  );
+}
--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./components/App";
 import "./index.css";
+import congigureStore from "./components/redux/configureStore";
+import { provider } from "react-redux";
 
+const store = congigureStore();
 render(
-  <Router>
-    <App />
-  </Router>,
+  <provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </provider>,
   document.getElementById("app")
 );
